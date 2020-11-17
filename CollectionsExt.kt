b5a0b2c21e402cd5b94063ca567f7af70dffb56b
Nail Asadullin <alxumeg@gmail.com>
2020-11-17 18:21:27 +0300

    added disctinctAndFilter and sumByDoubleCool methods

--- CollectionsExt.kt
+++ CollectionsExt.kt
@@ -5,3 +5,26 @@ fun <T> Set<T>?.orEmptyMutable(): MutableSet<T> = this?.toMutableSet() ?: mutabl
 fun <T> MutableCollection<T>.addIf(predicate: Boolean, whatToAdd: () -> T) {
     if (predicate) this.add(whatToAdd())
 }
+
+inline fun <T, K> Iterable<T>.distinctAndFilter(distinctBy: (T) -> K, filterBy: (T) -> Boolean): List<T> {
+    val set = HashSet<K>()
+    val list = ArrayList<T>()
+    for (e in this) {
+        val key = distinctBy(e)
+        if (set.add(key) && filterBy(e))
+            list.add(e)
+    }
+    return list
+}
+
+/**
+* Uses sumWith from DoubleExt to avoid algebraic errors
+* @see DoubleExt
+*/
+inline fun <T> Iterable<T>.sumByDoubleCool(selector: (T) -> Double): Double {
+    var sum: Double = 0.0
+    for (element in this) {
+        sum = sum.sumWith(selector(element))
+    }
+    return sum
+}
