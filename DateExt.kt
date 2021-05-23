83bc223650b8e4b51f2ac96ad46e0950629359fa
Nail Asadullin <alxumeg@gmail.com>
2021-05-23 14:09:10 +0300

    operator fun Date.minus

--- DateExt.kt
+++ DateExt.kt
@@ -2,7 +2,6 @@
 * With that you can write something like this: Date() + Date()
 */
 operator fun Date.plus(other: Date): Date = Date(this.time + other.time)
+operator fun Date.minus(other: Date): Date = Date(this.time - other.time)
 
-fun Date?.orNow(): Date {
-    return this ?: Date()
-}
+fun Date?.orNow() = this ?: Date()
